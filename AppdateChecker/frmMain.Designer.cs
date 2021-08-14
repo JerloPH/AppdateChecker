ec9d611a39c06c3da3715a31064eddcef43220c7
JerloPH <jewelcarlosalvador@gmail.com>
2021-08-14 11:49:10 +0800

    Updated UI looks

--- AppdateChecker/frmMain.Designer.cs
+++ AppdateChecker/frmMain.Designer.cs
@@ -31,6 +31,10 @@ namespace AppdateChecker
         {
             this.btnCheckUpdates = new System.Windows.Forms.Button();
             this.dgridApps = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.cbHideUpdated = new System.Windows.Forms.CheckBox();
             this.cImg = new System.Windows.Forms.DataGridViewImageColumn();
             this.cApp = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cRepoOwner = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -38,16 +42,12 @@ namespace AppdateChecker
             this.cCurVer = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cLatestVer = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colFilePath = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.btnSave = new System.Windows.Forms.Button();
-            this.btnRefresh = new System.Windows.Forms.Button();
-            this.cbHideUpdated = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgridApps)).BeginInit();
             this.SuspendLayout();
             // 
             // btnCheckUpdates
             // 
-            this.btnCheckUpdates.Location = new System.Drawing.Point(423, 345);
+            this.btnCheckUpdates.Location = new System.Drawing.Point(423, 411);
             this.btnCheckUpdates.Name = "btnCheckUpdates";
             this.btnCheckUpdates.Size = new System.Drawing.Size(159, 50);
             this.btnCheckUpdates.TabIndex = 2;
@@ -67,12 +67,55 @@ namespace AppdateChecker
             this.cCurVer,
             this.cLatestVer,
             this.colFilePath});
-            this.dgridApps.Location = new System.Drawing.Point(12, 44);
+            this.dgridApps.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgridApps.Location = new System.Drawing.Point(0, 0);
             this.dgridApps.Name = "dgridApps";
             this.dgridApps.RowHeadersWidth = 51;
             this.dgridApps.RowTemplate.Height = 29;
-            this.dgridApps.Size = new System.Drawing.Size(1010, 295);
+            this.dgridApps.Size = new System.Drawing.Size(1034, 391);
             this.dgridApps.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 411);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(131, 50);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(149, 411);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(131, 50);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save to DB";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(286, 411);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(131, 50);
+            this.btnRefresh.TabIndex = 7;
+            this.btnRefresh.Text = "Reload Entries";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // cbHideUpdated
+            // 
+            this.cbHideUpdated.AutoSize = true;
+            this.cbHideUpdated.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cbHideUpdated.Location = new System.Drawing.Point(597, 429);
+            this.cbHideUpdated.Name = "cbHideUpdated";
+            this.cbHideUpdated.Size = new System.Drawing.Size(207, 32);
+            this.cbHideUpdated.TabIndex = 8;
+            this.cbHideUpdated.Text = "Hide Updated Apps";
+            this.cbHideUpdated.UseVisualStyleBackColor = true;
+            this.cbHideUpdated.CheckedChanged += new System.EventHandler(this.cbHideUpdated_CheckedChanged);
             // 
             // cImg
             // 
@@ -127,49 +170,7 @@ namespace AppdateChecker
             this.colFilePath.HeaderText = "File Path";
             this.colFilePath.MinimumWidth = 6;
             this.colFilePath.Name = "colFilePath";
-            this.colFilePath.Width = 250;
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Location = new System.Drawing.Point(12, 345);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(131, 50);
-            this.btnAdd.TabIndex = 5;
-            this.btnAdd.Text = "Add";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-            // 
-            // btnSave
-            // 
-            this.btnSave.Location = new System.Drawing.Point(149, 345);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(131, 50);
-            this.btnSave.TabIndex = 6;
-            this.btnSave.Text = "Save to DB";
-            this.btnSave.UseVisualStyleBackColor = true;
-            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-            // 
-            // btnRefresh
-            // 
-            this.btnRefresh.Location = new System.Drawing.Point(286, 345);
-            this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(131, 50);
-            this.btnRefresh.TabIndex = 7;
-            this.btnRefresh.Text = "Reload Entries";
-            this.btnRefresh.UseVisualStyleBackColor = true;
-            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
-            // 
-            // cbHideUpdated
-            // 
-            this.cbHideUpdated.AutoSize = true;
-            this.cbHideUpdated.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.cbHideUpdated.Location = new System.Drawing.Point(12, 412);
-            this.cbHideUpdated.Name = "cbHideUpdated";
-            this.cbHideUpdated.Size = new System.Drawing.Size(207, 32);
-            this.cbHideUpdated.TabIndex = 8;
-            this.cbHideUpdated.Text = "Hide Updated Apps";
-            this.cbHideUpdated.UseVisualStyleBackColor = true;
-            this.cbHideUpdated.CheckedChanged += new System.EventHandler(this.cbHideUpdated_CheckedChanged);
+            this.colFilePath.Width = 280;
             // 
             // frmMain
             // 
@@ -197,6 +198,7 @@ namespace AppdateChecker
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.CheckBox cbHideUpdated;
         private System.Windows.Forms.DataGridViewImageColumn cImg;
         private System.Windows.Forms.DataGridViewTextBoxColumn cApp;
         private System.Windows.Forms.DataGridViewTextBoxColumn cRepoOwner;
@@ -204,7 +206,6 @@ namespace AppdateChecker
         private System.Windows.Forms.DataGridViewTextBoxColumn cCurVer;
         private System.Windows.Forms.DataGridViewTextBoxColumn cLatestVer;
         private System.Windows.Forms.DataGridViewTextBoxColumn colFilePath;
-        private System.Windows.Forms.CheckBox cbHideUpdated;
     }
 }
 
